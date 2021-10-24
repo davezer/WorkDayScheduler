8cb7a16477c67a17cd66eccfe19a15244e9b94b9
davezer <davezer27@gmail.com>
2021-10-23 20:38:59 -0400

    added function to add color to the blocks and to save items

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -3,3 +3,31 @@ var saveBtn = $(".saveBtn");
 
 // current day display at top of page
 $("#currentDay").text(moment().format('dddd, MMMM do YYYY'));
+
+// blocks color-coded to indicate if past present or future
+function blockColor(){
+    var hour = moment().hours();
+
+    $(".time-block").each(function(){
+        var currentHour = parseInt($(this).attr("id"));
+
+        console.log(this);
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour){
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+saveBtn.on("click", function(){
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan");
+
+    localStorage.setItem(time, plan);
+})
+
+blockColor();
