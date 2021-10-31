19ddd92a11e0aa94e6509c0d909231385c3e4614
davezer <davezer27@gmail.com>
2021-10-31 13:05:31 -0400

    got color blocks working correctly

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,34 +2,53 @@
 var saveBtn = $(".saveBtn");
 
 // current day display at top of page
-$("#currentDay").text(moment().format('dddd, MMMM do YYYY'));
+$("#currentDay").text(moment().format('LLLL'));
+
+//click event for save button and saving input
+saveBtn.on("click", function(){
+    var plan = $(this).siblings(".plan").val();
+    var time = $(this).siblings(".hour").text();
+    
+
+    localStorage.setItem(time, plan);
+});
+
+// $("#hour9 .plan").val(localStorage.getItem("hour9"));
+// $("#hour10 .plan").val(localStorage.getItem("hour10"));
+// $("#hour11 .plan").val(localStorage.getItem("hour11"));
+// $("#hour12 .plan").val(localStorage.getItem("hour12"));
+// $("#hour13 .plan").val(localStorage.getItem("hour13"));
+// $("#hour14 .plan").val(localStorage.getItem("hour14"));
+// $("#hour15 .plan").val(localStorage.getItem("hour15"));
+// $("#hour16 .plan").val(localStorage.getItem("hour16"));
+// $("#hour17 .plan").val(localStorage.getItem("hour17"));
 
 // blocks color-coded to indicate if past present or future
 function blockColor(){
-    var hour = moment().hours();
+    var currentHour = moment().hour();
 
-    $(".time-block").each(function(){
-        var currentHour = parseInt($(this).attr("id"));
+    $(".timeBlock").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
 
-        console.log(this);
+        //console.log(this);
 
-        if (currentHour > hour) {
-            $(this).addClass("future");
-        } else if (currentHour === hour){
+        if (blockHour < currentHour) {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        } else if (blockHour === currentHour){
+            $(this).removeClass("past");
+            $(this).removeClass("future");
             $(this).addClass("present");
         } else {
-            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
         }
     })
 }
 
-//click event for save button and saving input
-saveBtn.on("click", function(){
-    var time = $(this).siblings(".hour").text();
-    var plan = $(this).siblings(".plan").val();
 
-    localStorage.setItem(time, plan);
-})
 
 function planner(){
     $(".hour").each(function(){
@@ -39,7 +58,8 @@ function planner(){
         if (currentPlan !== null){
             $(this).siblings(".plan").val(currentPlan);
         }
-    })
+    });
+    
 }
 
 blockColor();
